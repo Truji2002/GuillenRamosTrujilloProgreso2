273a4f336f15fb3906e7057b0079a0cf083a18f2
David Antonio Trujillo Robalino <david.trujillo.robalino@udla.edu.ec>
2022-12-09 18:39:10 -0500

    j

---
+++ GuillenRamosTrujilloProgreso2/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GuillenRamosTrujilloProgreso2
+{
+    class Usuario
+    {
+        public string Correo { get; set; }
+        public string Contraseña { get; set; }
+    }
+}
