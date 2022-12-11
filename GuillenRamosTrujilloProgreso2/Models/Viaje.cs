44e616baf79a8106e68443670cd13e66b455a168
David Antonio Trujillo Robalino <david.trujillo.robalino@udla.edu.ec>
2022-12-10 19:16:07 -0500

    Listo el manejo de viajes

---
+++ GuillenRamosTrujilloProgreso2/Models/Viaje.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuillenRamosTrujilloProgreso2.Models
+{
+    class Viaje
+
+    {
+        public string Filename { get; set; }
+
+        public string Pais { get; set; }
+
+        public string Ciudad { get; set; }
+
+        public string NombreLugar { get; set; }
+
+        public string Resenia { get; set; }
+
+        public string Info { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
