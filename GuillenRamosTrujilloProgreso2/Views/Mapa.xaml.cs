1bb25b0013ee8a9525ada87960e200e0071788a9
Xavi122323 <xavier.ramos@udla.edu.ec>
2022-12-11 20:59:13 -0500

    Act Mapa

--- GuillenRamosTrujilloProgreso2/Views/Mapa.xaml.cs
+++ GuillenRamosTrujilloProgreso2/Views/Mapa.xaml.cs
@@ -1,9 +1,30 @@
+using Microsoft.Maui.Maps;
+
 namespace GuillenRamosTrujilloProgreso2.Views;
 
 public partial class Mapa : ContentPage
 {
-	public Mapa()
+
+
+    public Mapa()
 	{
-		InitializeComponent();
-	}
+        InitializeComponent();
+    }
+
+    void OnButtonClicked(object sender, EventArgs e)
+    {
+        Button button = sender as Button;
+        switch (button.Text)
+        {
+            case "Street":
+                map.MapType = MapType.Street;
+                break;
+            case "Satellite":
+                map.MapType = MapType.Satellite;
+                break;
+            case "Hybrid":
+                map.MapType = MapType.Hybrid;
+                break;
+        }
+    }
 }
