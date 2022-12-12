be9cb27ca2857d0667d003cb0c7f61d907ad3652
Xavi122323 <xavier.ramos@udla.edu.ec>
2022-12-11 22:06:50 -0500

    Act 3

--- GuillenRamosTrujilloProgreso2/Views/Mapa.xaml.cs
+++ GuillenRamosTrujilloProgreso2/Views/Mapa.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.Maui.Controls.Maps;
 using Microsoft.Maui.Maps;
 
 namespace GuillenRamosTrujilloProgreso2.Views;
@@ -26,5 +27,46 @@ public partial class Mapa : ContentPage
                 map.MapType = MapType.Hybrid;
                 break;
         }
+    }
+
+    void AgregarPin(object sender, EventArgs e)
+    {
+        double latitud = Double.Parse(Latitude.Text);
+        double longitud = Double.Parse(Longitude.Text);
+        string direccion = Address.Text;
+
+        Pin boardwalkPin = new Pin
+        {
+            Location = new Location(latitud, longitud),
+            Label = "",
+            Address = "",
+            Type = PinType.Place
+        };
+        boardwalkPin.MarkerClicked += OnMarkerClickedAsync;
+
+        Pin wharfPin = new Pin
+        {
+            Location = new Location(36.9571571, -122.0173544),
+            Label = "Wharf",
+            Address = "Santa Cruz",
+            Type = PinType.Place
+        };
+        wharfPin.InfoWindowClicked += OnInfoWindowClickedAsync;
+
+        map.Pins.Add(boardwalkPin);
+        map.Pins.Add(wharfPin);
+    }
+
+    async void OnMarkerClickedAsync(object sender, PinClickedEventArgs e)
+    {
+        e.HideInfoWindow = true;
+        string pinName = ((Pin)sender).Label;
+        await DisplayAlert("Pin Clicked", $"{pinName} was clicked.", "Ok");
+    }
+
+    async void OnInfoWindowClickedAsync(object sender, PinClickedEventArgs e)
+    {
+        string pinName = ((Pin)sender).Label;
+        await DisplayAlert("Info Window Clicked", $"The info window was clicked for {pinName}.", "Ok");
     }
 }
