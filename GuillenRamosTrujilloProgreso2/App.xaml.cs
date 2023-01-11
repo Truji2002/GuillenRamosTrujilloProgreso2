c40ffd196948f5ad3699ef4b754ba25c67bb1653
davidguillen2002 <david@LAPTOP-3KD5BQ0L>
2023-01-11 08:14:26 -0500

    Cambios

--- GuillenRamosTrujilloProgreso2/App.xaml.cs
+++ GuillenRamosTrujilloProgreso2/App.xaml.cs
@@ -6,8 +6,8 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-        MainPage = new AppShell();
+        //MainPage = new AppShell();
 
-        //MainPage = new NavigationPage(new MainPage());
+        MainPage = new NavigationPage(new MainPage());
     }
 }
