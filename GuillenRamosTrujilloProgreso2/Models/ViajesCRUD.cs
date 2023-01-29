bbcc97942a6208c0fa4d410eee0b1b5b875f3283
David Antonio Trujillo Robalino <david.trujillo.robalino@udla.edu.ec>
2023-01-28 19:41:15 -0500

    Validaciones

--- GuillenRamosTrujilloProgreso2/Models/ViajesCRUD.cs
+++ GuillenRamosTrujilloProgreso2/Models/ViajesCRUD.cs
@@ -1,4 +1,5 @@
 ﻿using SQLite;
+using System.ComponentModel.DataAnnotations;
 
 namespace GuillenRamosTrujilloProgreso2.Models
 {
@@ -8,16 +9,24 @@ namespace GuillenRamosTrujilloProgreso2.Models
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Ingrese el pais")]
+        [RegularExpression("^[a-zA-Z]*${1,100}", ErrorMessage = "Solo puede ingresar letras")]
         public string Pais { get; set; }
 
+        [Required(ErrorMessage = "Ingrese la ciudad")]
+        [RegularExpression("^[a-zA-Z]*${1,100}", ErrorMessage = "Solo puede ingresar letras")]
         public string Ciudad { get; set; }
 
+        [Required(ErrorMessage = "Ingrese el nombre del lugar")]
         public string NombreLugar { get; set; }
 
+        [Required(ErrorMessage = "Ingrese la reseña del lugar")]
         public string Resenia { get; set; }
 
+        [Required(ErrorMessage = "Ingrese información adicional del lugar")]
         public string Info { get; set; }
 
+        [Required(ErrorMessage = "Ingrese la fecha de su visista")]
         public DateTime Date { get; set; }
 
         public bool Servicio { get; set; }
